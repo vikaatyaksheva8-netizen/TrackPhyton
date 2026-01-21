8009219659c73387f26f4aed5394de9993f9a69f
vikaatyaksheva8-netizen <vikaatyaksheva8@gmail.com>
2026-01-21 20:00:41 +0300

    Лабораторная работа 2

---
+++ task1.py
@@ -0,0 +1,12 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+months = 0
+
+while money_capital + salary >= spend:
+    money_capital = money_capital + salary - spend
+    spend = spend * 1.05
+    months = months + 1
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
