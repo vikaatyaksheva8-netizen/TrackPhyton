5f090cea85fba42a7072bd793b3a8732a4d3a6e9
vikaatyaksheva8-netizen <vikaatyaksheva8@gmail.com>
2026-03-02 00:12:52 +0300

    Лабораторная работа 3

---
+++ task1.py
@@ -0,0 +1,63 @@
+class Book:
+    """Базовый класс Книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def author(self):
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self):
+        return self._pages
+    
+    @pages.setter
+    def pages(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Количество страниц должно быть целым числом")
+        if value <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = value
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages})"
+
+
+class AudioBook(Book):
+
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self):
+        return self._duration
+    
+    @duration.setter
+    def duration(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError("Продолжительность должна быть числом")
+        if value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self._duration = float(value)
+    
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration})"
